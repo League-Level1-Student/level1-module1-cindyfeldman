c07eba18b7c8a910aac305f7d9efb2fec43ec269
cindyfeldman <league@WTS-iMac-11.attlocal.net>
2018-04-14 14:27:51 -0700

    intro to objects

---
+++ src/Chocolate.java
@@ -0,0 +1,12 @@
+
+public class Chocolate {
+String type;
+int shape;
+public static void main(String[] args) {
+	
+}
+void melted() {
+	
+}
+void 
+}
---
+++ src/extra/duck.java
@@ -0,0 +1,21 @@
+package extra;
+
+public class duck {
+	int numberOfFriends;
+	String favoriteFood;
+	
+	public static void main(String[] args) {
+	duck name = new duck("crackers", 1);
+	System.out.println(name.favoriteFood);
+	}
+	
+void quack() {
+	
+}
+void waddle() {
+
+}
+ duck(String favoriteFood, int numberOfFriends) {
+  	this.favoriteFood = favoriteFood;
+  	this.numberOfFriends = numberOfFriends;
+}}
